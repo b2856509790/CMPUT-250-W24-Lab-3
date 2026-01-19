b3933071c95c9c5d70f1961753d71f9a30523b8e
LiamP <liammaxpbb@outlook.com>
2026-01-18 22:24:06 -0700

    1

--- CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
+++ CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class DVDLogo : MonoBehaviour
 {
     //Speed it moves at
-    public float speed = 3;
+    public float speed = 10;
 
     //Bounds of the screen (could get these with camera bounds but we can do this since it's a fixed camera)
-    public float X_Max = 5, Y_Max = 4;
+    public float X_Max = 4, Y_Max = 6;
 
     //Current direction
     private Vector3 direction;
